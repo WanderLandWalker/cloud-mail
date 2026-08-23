fb01ba8c794c9831578326746cb644a0b197b8de
WanderLandWalker <152128469+WanderLandWalker@users.noreply.github.com>
2026-08-24 00:22:43 +0800

    Update email-msg.js

--- mail-worker/src/template/email-msg.js
+++ mail-worker/src/template/email-msg.js
@@ -23,49 +23,42 @@ function truncateText(text, maxLength) {
 
 export default function emailMsgTemplate(email, tgMsgTo, tgMsgFrom, tgMsgText) {
 
-	let template = `<b>${email.subject}</b>`
 	let template = `<b>${escapeHtml(email.subject || '')}</b>`
 
-if (tgMsgFrom === 'only-name') {
-template += `
+		if (tgMsgFrom === 'only-name') {
+			template += `
 
-From\u200B：${email.name}`
 From\u200B：${escapeHtml(email.name || '')}`
-}
+		}
 
-if (tgMsgFrom === 'show') {
-template += `
+		if (tgMsgFrom === 'show') {
+			template += `
 
-From\u200B：${email.name}  &lt;${email.sendEmail}&gt;`
 From\u200B：${escapeHtml(email.name || '')}  &lt;${escapeHtml(email.sendEmail || '')}&gt;`
-}
+		}
 
-if(tgMsgTo === 'show' && tgMsgFrom === 'hide') {
-template += `
+		if(tgMsgTo === 'show' && tgMsgFrom === 'hide') {
+			template += `
 
-To：\u200B${email.toEmail}`
 To：\u200B${escapeHtml(email.toEmail || '')}`
 
-} else if(tgMsgTo === 'show') {
-template += `
-To：\u200B${email.toEmail}`
+		} else if(tgMsgTo === 'show') {
+		template += `
 To：\u200B${escapeHtml(email.toEmail || '')}`
-}
+	}
 
-	const text = (emailUtils.formatText(email.text) || emailUtils.htmlToText(email.content))
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;');
 	const text = escapeHtml(emailUtils.formatText(email.text) || emailUtils.htmlToText(email.content));
 
-if(tgMsgText === 'show') {
+	if(tgMsgText === 'show') {
 		const prefix = `${template}
 
 `;
 		const maxTextLength = Math.max(0, TELEGRAM_MESSAGE_LIMIT - prefix.length);
-template += `
+		template += `
 
-${text}`
 ${truncateText(text, maxTextLength)}`
-}
+	}
 
-return template;
+	return template;
+
+}
